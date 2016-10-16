503e66d8a91e2d4f9b527670b221cdbffdf875b1
Swojit Mohapatra <neelmohapatra@gmail.com>
2016-10-16 13:59:14 -0700

    Attempt to send the request to Google API

--- Dubhacks/JS/twitter.js
+++ Dubhacks/JS/twitter.js
@@ -4,6 +4,33 @@ var findParentInput = function(target) {
   console.log("Got target", target);
   var child = target.children[0];
   console.log(child.textContent);
+
+  var requestUrl = "https://language.googleapis.com/v1beta1/documents:analyzeSentiment?key=AIzaSyDvQ9hq2wcm6rzob0Sev4vMCZH30S3-5II";
+
+  var options = {
+     url: requestUrl,
+     type: "POST",
+     data: {
+        "document": {
+            "type": "PLAIN_TEXT",
+            "content": child.textContent
+        }
+     },
+     dataType:"json",
+     success: function(err, resp, body) {
+      if (!err && resp.statusCode == 200) {
+        console.log(body.sentiments);
+         //  var entities = body.entities;
+         //  for (index in entities) {
+         //      var entity = entities[index];
+         //      console.log(entity);
+         //  }
+      } else {
+          console.log("NL API error: ", err);
+      }
+    }
+  }
+  $.ajax(options);
 }
 
 var getInputText = function(parentInput) {
@@ -19,4 +46,4 @@ document.body.addEventListener("keydown", function() {
   currentStr = getInputText(parentInput);
 });
 
-console.log("running");
+// console.log("running");
